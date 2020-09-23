ce8a15626e89dd9c186e6291792011d560cf83e4
Luis Angel <santosangel748@gmail.com>
2020-09-22 19:58:40 -0500

    Configurando el navbar y otros componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/shared/home/home.component';
+import { AboutComponent } from './components/shared/about/about.component';
+import { HeroesComponent } from './components/shared/heroes/heroes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule
